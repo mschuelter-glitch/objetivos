b6bf0c4975c54c0c71803411b2d722137bed8d61
mschuelter-glitch <m.schuelter@escola.pr.gov.br>
2026-05-12 07:56:08 -0300

    .

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ for(let i = 0; i<botoes.length;i++){
             botoes[j].classList.remove("ativo")
             textos[j].classList.remove("ativo")
         }
-        botoes[j].classList.add("ativo")
-        textos[j].classList.add("ativo")
+        botoes[i].classList.add("ativo")
+        textos[i].classList.add("ativo")
     }
 }
